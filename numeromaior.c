16503da97dcc3971aeedc7b561e040ae6b7f6f0f
Gabriel Coelho Góes <161400667+Cogah@users.noreply.github.com>
2025-02-21 11:30:48 -0300

    Add files via upload

---
+++ numeromaior.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+	int i, aux = 0, length = 10;
+	int vet[length];
+
+	printf("Digite um numero: ");
+	scanf("%d", &vet[0]);
+	aux = vet[0];
+
+	for(i = 0; i < length; i++){
+		printf("\nDigite um numero: ");
+		scanf("%d",&vet[i]);	
+		if(vet[i] > aux){
+			aux = vet[i];
+		}
+	}
+
+	for(i = 0; i < length; i++){
+		if(i == length - 1) {
+			printf("%d\n",vet[i]);
+		}else {
+			printf("%d, ",vet[i]);
+		}
+	}
+
+	printf("\nO maior numero dessa sequwncia e %d\n", aux);
+	return 0;
+}
